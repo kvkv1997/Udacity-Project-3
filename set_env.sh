1c25bef48dd32d1c42bb8843c9c9adb1229e1a09
Liam Nguyen <tuhoang2916@gmail.com>
2022-06-13 15:46:40 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,22 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=liam
+export POSTGRES_PASSWORD=admin123
+export POSTGRES_HOST=liem-database-1.ceuylvtzmp2o.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=liampj3-325206099771-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
+
+setx POSTGRES_USERNAME liam
+setx POSTGRES_PASSWORD admin123
+setx POSTGRES_HOST liem-database-1.ceuylvtzmp2o.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB postgres
+setx AWS_BUCKET liampj3-325206099771-dev
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET hello
+setx URL http://localhost:8100
