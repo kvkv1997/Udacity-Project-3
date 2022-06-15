89270a707e51b8b6353f8b8bf7f0b3dfd20e9b1c
Liam Nguyen <tuhoang2916@gmail.com>
2022-06-15 09:42:52 +0700

    commit code to git

--- set_env.sh
+++ set_env.sh
@@ -7,16 +7,5 @@ export POSTGRES_DB=postgres
 export AWS_BUCKET=liampj3-325206099771-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=haison
 export URL=http://localhost:8100
-
-
-setx POSTGRES_USERNAME liam
-setx POSTGRES_PASSWORD admin123
-setx POSTGRES_HOST liem-database-1.ceuylvtzmp2o.us-east-1.rds.amazonaws.com
-setx POSTGRES_DB postgres
-setx AWS_BUCKET liampj3-325206099771-dev
-setx AWS_REGION us-east-1
-setx AWS_PROFILE default
-setx JWT_SECRET hello
-setx URL http://localhost:8100
